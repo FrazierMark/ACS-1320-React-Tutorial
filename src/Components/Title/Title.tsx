10f0385ba157ccbb501f78bc131f6e82e0c6e852
FrazierMark <m.mark.frazier@gmail.com>
2024-02-01 10:47:40 -0800

    Header component built

---
+++ src/Components/Title/Title.tsx
@@ -0,0 +1,12 @@
+import './Title.css';
+
+const Title = () => {
+	return (
+		<div className="Title">
+			<h1>SFPOPOS</h1>
+			<div className="Title-Subtitle">San Francisco Privately Owned Public Open Spaces</div>
+		</div>
+	);
+};
+
+export default Title;
